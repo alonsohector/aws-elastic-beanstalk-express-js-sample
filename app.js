66fa77ad26132fecc5f52e78dd3b17d2f01a5140
GitKrakenHectorAlonso <alonso.hector@gmail.com>
2021-08-30 23:56:28 -0500

    Adding changes for Review Step

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('From AWS Hello World! with more changes!'));
+app.get('/', (req, res) => res.send('From AWS Hello World! with more changes! using Review Step!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
