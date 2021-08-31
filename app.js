c08d9b85886e648b214b035aec8fc0ae0d939c41
GitKrakenHectorAlonso <alonso.hector@gmail.com>
2021-08-30 23:43:27 -0500

    Last for testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('From AWS Hello World! with changes!'));
+app.get('/', (req, res) => res.send('From AWS Hello World! with more changes!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
